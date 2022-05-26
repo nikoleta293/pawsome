8883ab8eb91ca082451cc813f6537aac43a3a4cd
GeorgeTs1 <54640117+GeorgeTs1@users.noreply.github.com>
2022-05-26 08:07:22 +0300

    Adding some stuff

--- Pawsome/Home/views.py
+++ Pawsome/Home/views.py
@@ -7,9 +7,27 @@ def show_prof_form(request):
     reg_form = RegistrationForm()
     pet_form = PetForm()
     pet_ok = request.POST.get('pet_ok')
-    context = {}
+    #context = {'ok_forms' : False}
+    ok_forms = False
 
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_ajax():
+        data = request.POST.get('data')
+        form = request.POST.get('form')
+        ok_forms = data.get('pet_ok')
+        
+        reg_form.fields['username'] = form.get('username')
+        reg_form.fields['email'] = form.get('email')
+        reg_form.fields['password'] = form.get('password')
+        reg_form.fields['password_confirm'] = form.get('password_confirm')
+
+        pet_form.fields['name'] = form.get('name')
+        pet_form.fields['age'] = form.get('age')
+        pet_form.fields['kind'] = form.get('kind')
+
+
+
+
+    if request.method == 'POST' and ok_forms == True:
         reg_form  = RegistrationForm(request.POST)
         pet_form  = PetForm(request.POST)
 
@@ -17,7 +35,6 @@ def show_prof_form(request):
             reg_form.clean_password()
             reg_form.save()
             pet_form.save()
-            context = { 'ok_forms' : True }
         
     
     return redirect('loginPage.html')   
