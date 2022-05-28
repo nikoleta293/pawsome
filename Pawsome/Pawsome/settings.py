d0850cbb1c2ff4a66cb1ba930d76ee86bf9c7779
GeorgeTs <54640117+GeorgeTs1@users.noreply.github.com>
2022-05-28 12:04:21 +0300

    adding MEDIA_ROOT MEDIA_URL

--- Pawsome/Pawsome/settings.py
+++ Pawsome/Pawsome/settings.py
@@ -134,3 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Users.Users'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Images')
+MEDIA_URL = '/Images/'
